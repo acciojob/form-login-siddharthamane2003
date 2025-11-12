a8c89f7175807fe2e0bc9f09206e9007da5ec48a
Siddharth Amane <111337001+siddharthamane2003@users.noreply.github.com>
2025-11-12 09:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
-    //Write your code here
+  // prevent form from reloading the page
+  event.preventDefault();
 
+  // Access the form
+  const form = document.forms["form1"];
+
+  // Get values of fname and lname
+  const fname = form["fname"].value.trim();
+  const lname = form["lname"].value.trim();
+
+  // Concatenate and show alert
+  const fullName = fname + " " + lname;
+  alert(fullName);
 }
